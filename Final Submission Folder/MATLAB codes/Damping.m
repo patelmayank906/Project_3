911e2998b5f6773634dd0a675a384e69012b0d7c
patelmayank906 <patel.479@wright.edu>
2016-12-09 21:44:00 -0500

    Updates

---
+++ Final Submission Folder/MATLAB codes/Damping.m
@@ -0,0 +1,18 @@
+%%The following code is used to calculate the C-matrix i.e. damping ratio
+
+function [C,fs] = Damping(K_r,M_r,zeta)
+OPTS.issym=1;
+  OPTS.isreal=1;
+  %To increase accuracy: (Agnes)
+  OPTS.tol=eps/10;
+  [minvals,minvallocs]=sort(diag(K_r)./diag(M_r));
+  shift=minvals(min(7,length(minvals)));
+  [fms,f]=eigs((K_r+K_r')/2+shift*(M_r+M_r')/2,(M_r+M_r')/2,min([ size(K_r,1) ...
+		    max([floor(sqrt(size(K_r,1))) 15])]),0,OPTS);
+  fs=sqrt(diag(f)-shift)/2/pi;
+  
+  zeta_omega1 = 2*zeta*fs*2*pi;
+  zeta_omega = diag(zeta_omega1);
+  C = fms'\zeta_omega/fms;
+  
+
---
+++ Final Submission Folder/MATLAB codes/stability.m
@@ -0,0 +1,11 @@
+function dT = stability(gamma,Beta,zeta,fs)
+omega = (zeta*(gamma-0.5)+sqrt((gamma/2) - Beta + zeta^2*(gamma-0.5)^2))/((gamma/2)-Beta);
+
+dT = omega/(max(fs));
+end
+
+
+
+
+
+
